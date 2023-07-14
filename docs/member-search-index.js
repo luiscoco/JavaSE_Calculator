162ef6b73b7697c8cd6f771388e342be9f12504c
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-14 11:20:26 +0200

    we delete the files in docs folder

--- docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"src","c":"Calculator","l":"add(double, double)","u":"add(double,double)"},{"p":"src","c":"Calculator","l":"Calculator()","u":"%3Cinit%3E()"},{"p":"src","c":"Calculator","l":"divide(double, double)","u":"divide(double,double)"},{"p":"src","c":"Calculator","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
