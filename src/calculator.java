fe785d218a5be166c67cf9cd8ca359940b5518cb
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-14 10:39:39 +0200

    added javadocs

--- src/calculator.java
+++ src/calculator.java
@@ -1,4 +1,32 @@
+package src;
+
 public class Calculator {
+     /**
+     * Adds two numbers.
+     *
+     * @param operand1 the first operand
+     * @param operand2 the second operand
+     * @return the sum of the two operands
+     */
+     public double add(double operand1, double operand2) {
+         return operand1 + operand2;
+    }
+
+    /**
+     * Divides one number by another.
+     *
+     * @param dividend the number to be divided
+     * @param divisor the number to divide by
+     * @return the result of the division
+     * @throws IllegalArgumentException if the divisor is zero
+     */
+    public double divide(double dividend, double divisor) {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero.");
+        }
+        return dividend / divisor;
+    }
+
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("Invalid number of arguments. Usage: java Calculator <operand1> <operator> <operand2>");
